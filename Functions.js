bf9a0fc499351fd9155814fc4fbfbfdada96dd36
unknown <diptik1408@gmail.com>
2023-12-04 16:35:30 +0530

    Functions class1

---
+++ Functions.js
@@ -0,0 +1,125 @@
+//    FUNCTIONS ::
+//        It consists of   :
+//        1. Function name 
+//        2. Function Body
+//        3. Optional return
+//        4. Function types 
+
+
+//  There are 2 SYNTAX of functions in JS ::
+   //1 SYNTAX :
+   //  Using function Keyword
+   // These is defined from the starting of JS.
+
+   //2 SYNTAX :
+   //  Arrow Functions  / FAT arrow function / ES function / Module Function
+
+   // This functions are firstly Described in ES6 -2015
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // (NO ARGS & NO RETURNS)
+
+   //EXAMPLE 1 : Using function keyword 
+
+    // function fun_name()
+    // {
+    //     console.log("I'm a function !")
+    //     alert ("I'm a function !")
+    // }
+    // fun_name()
+
+    //EXAMPLE 2. Using FAT arrow Functions 
+
+    // let fun_name=() =>
+    // {
+    //     console.log("I'm an arrow function")
+    // }
+    // fun_name()
+
+    
+
+    // ARGS BUT NO RETURN
+
+    //EXAMPLE 1 : Using FAT Arrow Function
+
+    // let  sum5=(num1 , num2) =>
+    // {
+    //     let addition = num1 + num2
+    //     console.log(addition)
+    // }
+
+    // sum5(50 , 50)
+
+    // EXAMPLE 2 : Using function Keyword
+
+    // function sum5 (num1 , num2)
+    // {
+    //     console.log(num1+num2)
+    // }
+    // sum5(20 , 20)
+
+    // ARGS AND RETURN :
+     
+    // EXAMPLE 1 : Using FAT arrow function
+
+    //    let sum5 =(num1 , num2) =>
+    //    {
+    //     let addition=num1 + num2
+    //     return addition 
+    //    }
+    //    let ans = sum5(50 , 20)
+    //    console.log(ans)
+    //    alert(ans)
+
+      //  console.log(sum5(25 , 25))
+
+        let sum5 = (num1 , num2)=>
+        {
+        
+            return num1 + num2
+        }
+        
+        console.log(sum5(20 ,15))
+
+
+        // EXAMPLE 2 : Using Function keyword
+        
+        function sum6(num1 , num2)
+        {
+            return num1 + num2
+        }
+        
+        console.log(sum6(200 ,15))
+
+        //NO ARGS BUT RETURN 
+        
+        // Example 1 : Using FAT arrow function
+
+        let fun1 =() =>
+        {
+            return "something"
+        }
+       console.log(fun1())
+
+       // EXAMPLE 2 : Using function keyword
+
+       function fun2()
+       {
+        return "anything"
+
+       }
+       console.log(fun2())
+
+
--- Looping.js
+++ Looping.js
@@ -87,15 +87,15 @@
 
     //Example 5 :: FACTORIAL OF A NUMBER 
     
-    let no = 6
-    let fact = 1
+    // let no = 6
+    // let fact = 1
 
-    while( no != 0)
-    {
-        fact = fact * no
-        no--
-    }
-    console.log(fact)
+    // while( no != 0)
+    // {
+    //     fact = fact * no
+    //     no--
+    // }
+    // console.log(fact)
     
 
     // Example 6 :: ARMSTRONG NUMBER 
@@ -107,12 +107,13 @@
 
            let n  = 153;
 
- let d;
+  let d;
 
- let org = n
- let sum = 0
+  let org = n
+  let sum = 0
 
- while(n != 0){
+ while(n != 0)
+ {
 
      d = n % 10
 
@@ -145,5 +146,103 @@
       d = num % 10
     
       console.log(d)
-    
 
+
+
+
+
+      //2. DO-WHILE LOOP :
+
+      //Syntax :
+      //   do {  
+      //          statements
+      //      }
+      //   while
+      //      (condition)
+
+
+      // Example ::
+
+              let c= 10
+              do{
+                console.log(c)
+                c++
+              }
+              while
+              (c <= 20)
+
+       
+       
+       //3.  FOR-LOOP ::
+
+       // Syntax :
+        
+       // for(initialize ; condition ; updation)
+       // {
+       //     console.log(statement) 
+       // }
+
+       // Example 1 :
+
+         for(let a1=0 ; a1<10 ; a1++)
+         {
+            console.log(a1)
+         }
+
+       // Example 2 :
+
+         for(let a2=10 ; a2>0 ; a2--)
+         {
+            console.log(a2)
+         }
+
+        // Example 3 :
+
+          for(let num1=5 ; num1<20 ; num1++)
+          {
+            if(num1 % 2 !=0)
+            {
+                console.log(num1 + " is a odd number")
+            }
+            else
+            {
+                console.log(num1 + " is a even number")
+            }
+          }
+
+          // Example 4 :
+
+            let fact = 1
+             
+            for(let nu=6  ; nu !=1 ; nu--)
+            {
+                fact= fact * nu
+                
+            }
+            console.log(fact)
+
+        
+        // // Example 5 :
+
+        let num1= 153
+        let d1
+        let org1 = num1
+        let sum1=0 
+
+        for(let num1= 153 ; num1 !=0 ;  num1=parseInt(num1/10) )
+        {
+            d1= num1 % 10
+            sum1 = sum1 + (d1 * d1 * d1)
+            
+        }
+        console.log(sum1)
+        
+        if(org1 == sum1)
+        {
+            console.log(sum1 + " is an armstrong number")
+        }
+        else
+        {
+            console.log(sum1 + " is not an armstrong number")
+        }
+            
